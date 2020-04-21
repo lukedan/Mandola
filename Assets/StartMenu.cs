772a9608e2cf7c463d50aa000f4b65033aee3efb
giaosame <qzcqs@icloud.com>
2020-04-21 15:40:58 -0400

    add start menu ui

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    // Start to play the game in opposing mode
+    public void PlayOppsingMode()
+    {
+        SceneManager.LoadScene("NetworkScene");
+    }
+
+    // Start to play the game in grouping mode
+    public void PlayGroupingMode()
+    {
+    	// Change this to the "GroupModeScene" after implementing it.
+        SceneManager.LoadScene("NetworkScene");
+    }
+
+    // Quit the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
