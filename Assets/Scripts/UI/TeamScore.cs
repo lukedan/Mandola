9fda24c8a630f7ef8545f7e414fb1f11402ca855
HaoyuSui <haoyusui@vip.163.com>
2020-04-26 22:33:49 -0400

    team score UI

---
+++ Assets/Scripts/UI/TeamScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamScore : MonoBehaviour
+{
+    public GameObject scoreCanvasPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Teams teams = InGameCommon.CurrentGame.GetComponent<Teams>();
+        for (int i = 0; i < teams.Colors.Count; ++i)
+        {
+            GameObject canvas = Instantiate(scoreCanvasPrefab, transform);
+            TeamScoreCanvas logic = canvas.GetComponent<TeamScoreCanvas>();
+            logic.TeamColor = teams.Colors[i];
+            logic.TeamIndex = i;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TeamScoreCanvas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeamScoreCanvas : MonoBehaviour
+{
+    public Color TeamColor;
+    public int TeamIndex;
+
+    public Text scoreText;
+
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.color = TeamColor;
+        scoreText.text = string.Format("Team{0} Score: " + score, TeamIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Teams teams = InGameCommon.CurrentGame.GetComponent<Teams>();
+        score = teams.Scores[TeamIndex];
+        scoreText.text = string.Format("Team{0} Score: " + score, TeamIndex + 1);
+    }
+}
