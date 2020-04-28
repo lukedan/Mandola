9b19de753187c289914fb654f60b2a277c3ebca5
Xuanyi Zhou <zxy19980101@sina.cn>
2020-04-27 21:51:05 -0400

    fix duplicate players, take two

--- Assets/Scripts/Player/PlayerGeneralInfo.cs
+++ Assets/Scripts/Player/PlayerGeneralInfo.cs
@@ -42,7 +42,7 @@ public class PlayerGeneralInfo : MonoBehaviour, IPunInstantiateMagicCallback {
 	private void Start() {
 		_network = GetComponent<PhotonView>();
 		InGameCommon.CurrentGame.GlobalPostProcessing.profile.TryGetSettings(out _damageEffect);
-		InGameCommon.CurrentGame.PlayerAvatars[_network.Owner.ActorNumber] = gameObject;
+		InGameCommon.CurrentGame.PlayerAvatars[_network.OwnerActorNr] = gameObject;
 	}
 
 	private void Update() {
